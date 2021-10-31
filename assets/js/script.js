c37d17b1ad3f5ad9cce8e3d7f6159ea68bd20b60
santiago <72000936+santiagov916@users.noreply.github.com>
2021-10-31 12:02:37 -0700

    start button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,71 @@
-startup = function() {
-    $("main").innerhtml("<div></div>");
-    innerhtml("")
-}
+const quizQ = [
+    {
+        question: "Commmonly used database types DO NOT include:",
+        a: "strings",
+        b: "booleans",
+        c: "alerts",
+        d: "numbers",
+        correctAnswer: "c",
+    },
+    {
+        question: "the condition of an if/else statement is enclosed with ________.",
+        a: "quotes",
+        b: "curly brackets",
+        c: "parenthesis",
+        d: "square brackets",
+        correctAnswer: "b",
+    },
+    {
+        question: "Arrays in javascript can be used to store _______.",
+        a: "numbers and strings",
+        b: "other arrays",
+        c: "booleans",
+        d: "all of the above",
+        correctAnswer: "d",
+    },
+    {
+        question: "What year was Javascript finally opene to the public",
+        a: "1995",
+        b: "1998",
+        c: "1994",
+        d: "2001",
+        correctAnswer: "a",
+    },
+    {
+        question: "HTML is an acronym for:",
+        a: "How To Make Language",
+        b: "Hyper Text Mastery Language",
+        c: "HyperText Markup Language",
+        d: "Hyper Tone Markup Language",
+        correctAnswer: "c",
+    },
+    {
+        question: "What does CSS stand for:",
+        a: "Cascading Style Series",
+        b: "Central Series of Sheets",
+        c: "Creative Stylistic Strategies",
+        d: "Cascading Style Sheets",
+        correctAnswer: "d",
+    },
+];
 
-startup();
+const start = document.getElementsByClassName('start')
+const answerChoice = document.querySelectorAll('answer')
+const quizFront = document.getElementById('quiz-front')
+const question = document.getElementById('quiz-frame')
+const aText = document.getElementsByClassName('a-text')
+const bText = document.getElementsByClassName('b-text')
+const cText = document.getElementsByClassName('c-text')
+const dText = document.getElementsByClassName('d-text')
+const submit = document.getElementById('final-answer')
+
+let currentQuiz = 0
+let score = 0
+
+
+    $(start).on('click', function() {
+        console.log("yes");
+        $(question).removeClass("quiz-body");
+        $(quizFront).addClass("quiz-body");
+        
+    });
